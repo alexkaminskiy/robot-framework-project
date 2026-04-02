794180c830ca313a5068bf8e7ef4397df918f1b5
Oleksandr Kaminskyi (EXT-Nokia) <oleksandr.kaminskyi.ext@nokia.com>
2026-04-02 12:16:42 +0300

    Debug CI

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 export PYTHONPATH=$(pwd)
-poetry run robot tests/api
+poetry run robot --outputdir /tests/reports tests/api
+
+echo "Listing /tests"
+ls -la /tests
+
+echo "Listing /tests/reports"
+ls -la /tests/reports
